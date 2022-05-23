44a9eff61b0c3b465347a5d4cfee18c6ef55ac19
dhavalprotonshub <93986143+dhavalprotonshub@users.noreply.github.com>
2022-05-23 13:04:03 +0530

    Done first screen

--- MyAwesomeProject/src/Route/index.tsx
+++ MyAwesomeProject/src/Route/index.tsx
@@ -1,7 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
-
+import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../Screens/Home';
+import { HomeScreenName } from './FileNames';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +10,7 @@ export const GetHomeStack=()=>{
     return (
         <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name={HomeScreenName} component={Home} />
         </Stack.Navigator>
       </NavigationContainer>
     )
